dad071ad118ac6666f965b4dd4f2606d859b6091
BY-Plus <1392998434@qq.com>
2020-06-17 19:11:34 +0800

    6.17

---
+++ js/2test.js
@@ -0,0 +1,27 @@
+var person = {
+    name:'aaa'
+}
+
+//需求 既要看到 person 具体内容 又要阻止下面代码执行
+// 打断调试----watch 监听某一个变量 是否声明 以及赋值的过程
+// 没打声明 no available 有声明 初始 underdined 值得的变化
+console.log(person)
+var person = 5
+alert(person)
+console.log('===========')
+var _name = 'gy'
+alert(_name)
+var num = 4
+alert (num)
+var num1 = 3
+alert(num1)
+var sum = num = num1
+
+var true+false
+console.log(true+false)
+
+console.log( 2>1 && 'gu>liu' && 'true+flase')
+
+abc>aabc
+
+
